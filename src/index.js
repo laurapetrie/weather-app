4f49c46e60bea328f6db357d7a1c0ede2f4768ce
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2022-11-23 10:08:26 +0000

    Updated the time and switched the API to sheCodes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-//Search engine
-
 //Search for a city
 function citySearch(event) {
   event.preventDefault();
@@ -14,8 +12,8 @@ let form = document.querySelector("#city-search");
 form.addEventListener("submit", citySearch);
 
 function searchCity(cityInput) {
-  let apiKey = "4ceb52be1d2505a597cef32a455231ba";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+  let apiKey = "e7cba0f4344b9ae720f19t5d48co46c3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
 
@@ -35,8 +33,6 @@ function displayWeather(response) {
   let wind = Math.round(response.data.wind.speed);
   currentWind.innerHTML = `Windspeed: ${wind} mph`;
 }
-// Adding the searchCity function outside of the other functions means that it will search for London on page load
-searchCity("London");
 
 // Current location button
 
@@ -49,8 +45,9 @@ let currentButton = document.querySelector("#current-location-button");
 currentButton.addEventListener("click", currentLocation);
 
 function currentPreciseLocation(position) {
-  let apiKey = "4ceb52be1d2505a597cef32a455231ba";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiKey = "e7cba0f4344b9ae720f19t5d48co46c3";
+  // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${position.coordinates.latitude}&lon=${position.coordinates.longitude}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayWeather);
 }
@@ -60,7 +57,13 @@ function currentPreciseLocation(position) {
 let now = new Date();
 let date = now.getDate();
 let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
 let days = [
   "Sunday",
@@ -95,16 +98,5 @@ todaysDate.innerHTML = `${day} ${date} ${month}`;
 let currentTime = document.querySelector("#current-time");
 currentTime.innerHTML = `${hour}:${minutes}`;
 
-// function changeCelsius() {
-//   let currentCelsius = document.querySelector("#daily-temp");
-//   currentCelsius.innerHTML = 17;
-// }
-// let clickCelsius = document.querySelector("#current-temp-celsius");
-// clickCelsius.addEventListener("click", changeCelsius);
-
-// function changeFahrenheit() {
-//   let currentFahrenheit = document.querySelector("#daily-temp");
-//   currentFahrenheit.innerHTML = 63;
-// }
-// let clickFahrenheit = document.querySelector("#current-temp-fahrenheit");
-// clickFahrenheit.addEventListener("click", changeFahrenheit);
+// searchCity function outside of the other functions will run on page load
+searchCity("London");
